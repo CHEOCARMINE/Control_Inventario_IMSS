c32a4803f4e3df21999b3cebb684459d3f298ca3
CHEOCARMINE <cheo_romero2001@live.com.mx>
2025-06-16 13:40:26 -0600

    chore(settings): configurar WhiteNoise para servir archivos estáticos
    - Añade `STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'` en settings.py

--- Inventario_IMSS/settings.py
+++ Inventario_IMSS/settings.py
@@ -68,6 +68,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'Inventario_IMSS.urls'
 
 TEMPLATES = [
